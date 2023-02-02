b8d32a5f55fb44d78381185c90be33e7b1ab455b
Daniel Marton <instantbuddha@gmail.com>
2023-02-02 11:10:13 +0100

    Typify Paginator

--- src/Paginator.tsx
+++ src/Paginator.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 
-function Paginator(props) {
-  const leftArrow = "<--";
-  const rightArrow = "-->";
+type PaginatorProps = {
+  currentPage: number;
+  changeCurrentPage: (isAddition: boolean) => void;
+};
+
+function Paginator(props: PaginatorProps) {
+  const LEFT_ARROW = "<--";
+  const RIGHT_ARROW = "-->";
   return (
     <div>
       <button onClick={() => props.changeCurrentPage(false)}>
-        {leftArrow}
+        {LEFT_ARROW}
       </button>
       <button onClick={() => props.changeCurrentPage(true)}>
-        {rightArrow}
+        {RIGHT_ARROW}
       </button>
     </div>
   );
